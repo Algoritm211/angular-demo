c9cf5ab3a985131a38720f6a39d23d952426d0a7
Alexey <algoritm211@gmail.com>
2022-09-17 10:15:44 +0300

    feat: add displaying heroes' mock information

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../types/hero";
+import {HEROES} from "../mocks/mock-heroes";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 221,
-    name: 'Alex',
-  }
+
+  heroes: Hero[] = HEROES
+  selectedHero?: Hero
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectHero(hero: Hero) {
+    this.selectedHero = hero;
   }
 
 }
